d285fa6b512170197af415cd3946259bd6a8cddc
Tri Wulandari <112963736+twdtrii@users.noreply.github.com>
2024-09-01 19:27:17 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/main.css";
 import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 import {BrowserRouter} from "react-router-dom";
 
